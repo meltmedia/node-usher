4f7aa771653aa74bb258ab9b33080ff14175fe81
Mike Moulton <mike@meltmedia.com>
2015-04-10 22:28:29 -0700

    Increasing maxSockets on default agent to speed up tests

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,12 @@
 var winston = require('winston'),
+    http = require('http'),
+    https = require('https'),
     AWS = require('aws-sdk'),
     fixtures = require('./fixtures');
+
+// Bump up the default of 5 so we can do a lot more work at once
+http.globalAgent.maxSockets = 500;
+https.globalAgent.maxSockets = 500;
 
 // Remove console transport unless we are debugging
 winston.remove(winston.transports.Console);
@@ -12,6 +18,7 @@ AWS.config.update({
   region: 'us-west-1',
   sslEnabled: true,
   httpOptions: {
+    agent: https.globalAgent,
     timeout: 70000 // Long poll is 60, so 70 should catch problems
   }
 });
